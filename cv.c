cca81f985b7cc506bba9e544508db2456f17a412
zx <1974625547@qq.com>
2018-03-10 19:57:08 +0800

    cv(r=1/2,k=7)

--- cv.c
+++ cv.c
@@ -1,7 +1,7 @@
-// 卷积编码(r=1/2,k=3)
-// g1= 1 1 1
-// g2= 1 0 1
-//　输入l bit，输出(2*l+4)bit，在信息序列较长的时候接近于1/2
+// 卷积编码(r=1/2,k=7)
+// g1= 1 1 1 1 0 0 1
+// g2= 1 0 1 1 0 1 1
+//　输入l bit，输出(2*l+(k-1)*2)bit，在信息序列较长的时候接近于1/2
 // 当前程序给每bit分配了1个int的空间，可改进
 
 #include <stdio.h>
@@ -9,14 +9,14 @@
 #include <string.h>
 
 int cv_code(int *buff_in,int k,int *buff_out);
-int move(int *buff);
+int move(int *buff);//右移
 
 int main()
 {
-	int m[3]={1,0,1};
+	int m[7]={1,1,1,1,1,1,1,};//读入数据
 	//
-	int out[10];//
-	cv_code(m,3,out);
+	int out[26];//
+	cv_code(m,7,out);//k=7
 
 	// for(int i=0;i<10;i++)
 	// {
@@ -38,15 +38,15 @@ int cv_code(int *buff_in,int k,int *buff_out)
     {
     	move(temp);
     	temp[0]=buff_in[i];
-    	buff_out[count++]=(temp[0]+temp[1]+temp[2])%2;//
-    	buff_out[count++]=(temp[0]+temp[2])%2;//
+    	buff_out[count++]=(temp[0]+temp[1]+temp[2]+temp[3]+temp[6])%2;//
+    	buff_out[count++]=(temp[0]+temp[2]+temp[3]+temp[5]+temp[6])%2;//
     }
     for(int i=0;i<len-1;i++)
     {
     	move(temp);
     	temp[0]=0;
-    	buff_out[count++]=(temp[0]+temp[1]+temp[2])%2;//
-    	buff_out[count++]=(temp[0]+temp[2])%2;//
+    	buff_out[count++]=(temp[0]+temp[1]+temp[2]+temp[3]+temp[6])%2;//
+    	buff_out[count++]=(temp[0]+temp[2]+temp[3]+temp[5]+temp[6])%2;//
     }
 
 	free(temp);
